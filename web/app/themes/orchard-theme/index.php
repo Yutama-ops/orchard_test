5cb03ed9ee605e30c80f22538583c89d6dc0658c
yutama <tamcrush@yahoo.com>
2024-03-19 22:09:52 +1100

    fix acf
    fixing acf for cleaner code,
    change color and background

--- web/app/themes/orchard-theme/index.php
+++ web/app/themes/orchard-theme/index.php
@@ -6,35 +6,32 @@ get_header();
         <main id="main" class="site-main home">
 
 	        <?php
+				
 				$title = get_field('title');
 
 				$content = get_field('content');
-				preg_match_all('/<p[^>]*>(.*?)<\/p>/', $content, $matches);
-				$paragraphs = $matches[1];
-
-
-				$images_content = get_field('images');
-				$images = array();
-				preg_match_all('/<img[^>]+src="([^">]+)"/', $images_content, $matches);
-				if(!empty($matches) && !empty($matches[1])){
-    				$images = $matches[1];
-				}
+				
+				// can build a cleaner php if i have ACF pro
+				$image_1 = get_field('image_1');
+				$image_2 = get_field('image_2');
+				$image_3 = get_field('image_3');
+				
 			?>
 			<div class="container main-content my-9">
 			<div class="row g-3 mb-4 d-flex align-items-stretch">
 				<div class="col-6 col-lg-4 d-flex">
-					<img src="<?php echo $images[0]; ?>" class="img-fluid w-100 align-self-stretch" alt="Nature Image">
+					<img src="<?php echo $image_1; ?>" class="img-fluid w-100 align-self-stretch" alt="Nature Image">
 				</div>
 				<div class="col-6 col-lg-4 d-flex flex-column justify-content-between">
-					<img src="<?php echo $images[1]; ?>" class="img-fluid mb-2" alt="Food Image">
-					<img src="<?php echo $images[2]; ?>" class="img-fluid" alt="Asparagus Image">
+					<img src="<?php echo $image_2; ?>" class="img-fluid mb-2" alt="Food Image">
+					<img src="<?php echo $image_3; ?>" class="img-fluid" alt="Asparagus Image">
 				</div>
 				<div class="col-12 col-lg-4 d-flex flex-column pt-1">
 					<div class="mb-2">
 						<h2 class="fw-light text-uppercase border-bottom border-secondary pb-1 mb-2"><?php echo $title; ?></h2>
-						<p class="pb-2"><?php echo $paragraphs[0]; ?></p>
-						<h6 class="pb-2 text-danger"><?php echo $paragraphs[1]; ?></h6>
-						<p class="fw-bold pb-2"><?php echo $paragraphs[2]; ?></p>
+						<?php if(isset($content)) {
+							echo $content; 
+						}?>
 					</div>
 				</div>
 			</div>
